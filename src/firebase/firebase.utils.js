935f98cac2703ea4a523c9269e6db0385ef87131
Priyam <priyam009@live.com>
2020-07-15 19:59:22 +1000

    Adding config for firebase

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyAjO4ROfcHJ5BsgGf7_7Lwz4MoJokxxO9M",
+  authDomain: "crown-db-115da.firebaseapp.com",
+  databaseURL: "https://crown-db-115da.firebaseio.com",
+  projectId: "crown-db-115da",
+  storageBucket: "crown-db-115da.appspot.com",
+  messagingSenderId: "679390045510",
+  appId: "1:679390045510:web:89ee183fe47803ed05b147",
+  measurementId: "G-4XD8TLSVQN",
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
