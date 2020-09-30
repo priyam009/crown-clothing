8ae92131875fe0af194566fe06dbbfaaeed4d022
Priyam <priyam009@live.com>
2020-10-01 07:33:20 +1000

    Update Stripe intergration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51HXCIpIvlZzmcbAlkiT3zXmqAKxyLJOz45v4nKQe5fUie6QZLh5Z0UyGPszRey2aF0j3zfXKxp538SHhFDxZ4Ty700BTlojCAc";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CROWN Clothing"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
